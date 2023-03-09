d52449b705f7da29edcce3fedffbc4f126f0528f
Vinigperuzzi <vinigperuzzi@hotmail.com>
2023-03-09 02:16:38 -0300

    Cores!!

--- fundamentals/setting_up/scriptComp.js
+++ fundamentals/setting_up/scriptComp.js
@@ -9,9 +9,6 @@ in vec2 a_position;
 // Used to pass in the resolution of the canvas
 uniform vec2 u_resolution;
 
-in vec4 a_color;
-out vec4 v_color;
-
 // all shaders have a main function
 void main() {
 
@@ -25,46 +22,72 @@ void main() {
   vec2 clipSpace = zeroToTwo - 1.0;
 
   gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-  v_color = a_color;
 }
 `;
-
 var fragmentShaderSource = `#version 300 es
 
 // fragment shaders don't have a default precision so we need
 // to pick one. highp is a good default. It means "high precision"
 precision highp float;
-in vec4 v_color;
 
 // we need to declare an output for the fragment shader
 out vec4 outColor;
 
 void main() {
   // Just set the output to a constant redish-purple
-  outColor = v_color;
+  outColor = vec4(1, 0, 0.5, 1);
 }
 `;
 
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));  // eslint-disable-line
+  gl.deleteShader(shader);
+  return undefined;
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));  // eslint-disable-line
+  gl.deleteProgram(program);
+  return undefined;
+}
+
 function main() {
   // Get A WebGL context
-  var canvas = document.querySelector("#c");
+  var canvas = document.querySelector("#glcanvas");
   var gl = canvas.getContext("webgl2");
   if (!gl) {
     return;
   }
 
-  // Use our boilerplate utils to compile the shaders and link into a program
-  var program = webglUtils.createProgramFromSources(gl, [vertexShaderSource, fragmentShaderSource]);
+  // create GLSL shaders, upload the GLSL source, compile the shaders
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  // Link the two shaders into a program
+  var program = createProgram(gl, vertexShader, fragmentShader);
 
   // look up where the vertex data needs to go.
   var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  var colorLocation = gl.getAttribLocation(program, "a_color");
 
-  // look up uniform locations
   var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
 
-  // Create a buffer and put a single pixel space rectangle in
-  // it (2 triangles)
   // Create a buffer and put three 2d clip space points in it
   var positionBuffer = gl.createBuffer();
 
@@ -73,11 +96,8 @@ function main() {
 
   var positions = [
     0, 0,
-    0, 150,
-    300, 0,
-    0, 150,
-    300, 0,
-    300, 150,
+    0, 500,
+    300, 250,
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
@@ -96,22 +116,8 @@ function main() {
   var normalize = false; // don't normalize the data
   var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
   var offset = 0;        // start at the beginning of the buffer
-  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
-
-  // Create a buffer for the colors.
-  var buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  
-  // Set the colors.
-  setColors(gl);
-
-  gl.enableVertexAttribArray(colorLocation);
-  var size = 4;
-  var type = gl.FLOAT;
-  var normalize = false;
-  var stride = 0;
-  var offset = 0;
-  gl.vertexAttribPointer(colorLocation, size, type, normalize, stride, offset);
+  gl.vertexAttribPointer(
+      positionAttributeLocation, size, type, normalize, stride, offset);
 
   webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
@@ -120,7 +126,7 @@ function main() {
 
   // Clear the canvas
   gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
@@ -128,37 +134,14 @@ function main() {
   // Bind the attribute/buffer set we want.
   gl.bindVertexArray(vao);
 
-  // Pass in the canvas resolution so we can convert from
-  // pixels to clipspace in the shader
   gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
 
   // draw
   var primitiveType = gl.TRIANGLES;
   var offset = 0;
-  var count = 6;
+  var count = 3;
   gl.drawArrays(primitiveType, offset, count);
 }
 
 main();
-
-function setColors(gl) {
-  // Pick 2 random colors.
-  var r1 = Math.random();
-  var b1 = Math.random();
-  var g1 = Math.random();
- 
-  var r2 = Math.random();
-  var b2 = Math.random();
-  var g2 = Math.random();
- 
-  gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array(
-        [ r1, b1, g1, 1,
-          r1, b1, g1, 1,
-          r1, b1, g1, 1,
-          r2, b2, g2, 1,
-          r2, b2, g2, 1,
-          r2, b2, g2, 1]),
-      gl.STATIC_DRAW);
-}
